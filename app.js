316ce7fab3905742081c47236ad2c28959d350d6
matidiaz032 <maty_032@hotmail.es>
2021-10-31 11:47:51 -0300

    end forms

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.get('/', function (req, res){
 app.get('/login', function (req, res){
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
+app.get('/register', function (req, res){
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
 app.get('/productDetail', function (req, res){
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
 });
