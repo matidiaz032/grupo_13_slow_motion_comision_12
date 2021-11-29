11154d5a228b63691e8207dd03f7fbb20a8cb483
AngelMontania <AngelGuillermoMontaa@gmail.com>
2021-11-29 18:33:58 -0300

    Modified routes

--- src/controllers/usersDataController.js
+++
@@ -1,9 +0,0 @@
-let controller = {
-    index: (req, res) => {
-        res.render('./admin/motionUsers', {
-            title: 'Admin - Page : Users'
-        })
-    }
-}
-
-module.exports = controller
--- src/routes/adminMovieRouter.js
+++
@@ -1,7 +0,0 @@
-let express = require('express');
-let router = express.Router();
-const controller = require('../controllers/adminMovieController');
-
-router.get('/', controller.index);
-
-module.exports = router;
