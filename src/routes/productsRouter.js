443fefa356cb9187ea7f2298c9655220ace421de
Galeano Elias <galeanoeliasdavid@gmail.com>
2021-12-23 16:57:12 -0300

    style-cart

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,11 +1,11 @@
- let express = require('express');
+let express = require('express');
 let router = express.Router();
 const controller = require('../controllers/productsController.js');
 
 router.get('/movies', controller.movies);
 router.get('/series', controller.series);
 router.get('/serialMovie/:id', controller.serialMovie);
-router.get('/serialSerie/:id', controller.serialSerie   )
+router.get('/serialSerie/:id', controller.serialSerie);
 router.get('/cart', controller.cart);
 
 module.exports = router;
