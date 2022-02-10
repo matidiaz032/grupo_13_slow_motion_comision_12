58b062a90599a582937154ae674d5ccf3d4190cf
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2022-02-10 15:49:05 -0300

    full at 85% carrusel principal home

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,4 +1,3 @@
-
 var angle = 0;
 function galleryspin(elem) { 
     spinner = document.querySelector("#spinner");
@@ -45,3 +44,50 @@ series.on('panright', () => {
         count += 1
     }
 })
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+let elemCarousel = document.querySelector('.slideshow-container')
+var carousel = new Hammer(elemCarousel)
+
+
+carousel.on('panleft', (event) => {
+    if(event.isFinal) {
+        plusSlides(1)
+    }
+})
+carousel.on('panright', (event) => {
+    if(event.isFinal) {
+        plusSlides(-1)
+    }
+})
+
+
