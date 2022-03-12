162b22fb88aa0ab88a6e434d1625915d7642f9e5
matidiaz032 <maty_032@hotmail.es>
2022-03-11 22:55:49 -0300

    end front change avatar & modal

--- public/js/newAvatar.js
+++ public/js/newAvatar.js
@@ -5,16 +5,27 @@ function $(elem) {
 window.addEventListener('load', () => {
     let btnAvatar = $('#btnAdd');
     let divAvatar = $('#divAvatar');
+    let inputAvatar = $('#changeAvatar');
+    let modal = $('#bg-modal');
+    let btnCancel = $('#btnCancel');
     
     divAvatar.addEventListener('mouseover', e => {
-        console.log("focus");
         btnAvatar.classList.add('btn-change-avatar');
         btnAvatar.classList.remove('btn-hover');
     })
 
     divAvatar.addEventListener('mouseout', e => {
-        console.log("endfocus");
         btnAvatar.classList.add('btn-hover');
         btnAvatar.classList.remove('btn-change-avatar');
+    })
+
+    inputAvatar.addEventListener('change', e => {
+        console.log('Cambio de img');
+        modal.classList.remove('d-none');
+        modal.classList.add('bg-modal');
+    })
+
+    btnCancel.addEventListener('click', e => {
+        modal.style.display = "none"
     })
 })
