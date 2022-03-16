3dbda2f77b58e040932320c8eb1dce5e1a54e5de
Galeano Elias <galeanoeliasdavid@gmail.com>
2022-03-16 15:23:04 -0300

    Admin : Menu

---
+++ public/js/menuAdmin.js
@@ -0,0 +1,22 @@
+// function qs(element) {
+//     return document.querySelector(element)
+// };
+
+// window.addEventListener('load', function () {
+
+//     let selectForm = qs('#adminMoviesSection');
+
+//     let optionMovies = qs('#optionMovies');
+
+//     let optionFormMovie = qs('#optionFormMovie');
+
+//     selectForm.addEventListener('onClick', function() {
+//         if(selectForm.value === "optionMovies"){
+//             window.location.assign('../admin/form')
+//         } else if(selectForm.value === "optionFormMovie"){
+//             window.location.assign('../admin/form')
+//         }else{
+//             window.location.href()
+//         }
+//     })
+// })
